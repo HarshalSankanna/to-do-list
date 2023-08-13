a381ddc726d883fcff4120bb92591973484310d3
Harshal <harshal0921@gmail.com>
2023-08-13 15:14:18 +0530

    Initialize state and create todo component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 function App() {
-  return <TodoList />;
+  const [todos, setTodos] = useState([]);
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add To-do</button>
+      <button>Clear Completed To-do</button>
+      <div>Left to do</div>
+    </>
+  );
 }
 
 export default App;
